608488304718c29a34f15c064dbe1679b76cbc2a
Yousef Yacoub <YousefYacoub@Yousefs-MacBook-Pro.local>
2023-12-28 13:56:55 -0500

    completed task1

--- client_test.py
+++ client_test.py
@@ -9,6 +9,7 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
 
+
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
